75ee96326ea789fa9235ef4eb38f9b1c603fad89
Henning Summer <115693958+henning-nr@users.noreply.github.com>
2026-03-12 00:23:07 +0000

    criando o busca ruas parcial mostrando a lista de ruas

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-function mostrar(){
+// Função de buscar por CEP
+function mostrar() {
 	cep = document.getElementById("cep").value // pegando valor do cep
 	// url = "https://viacep.com.br/ws/"+cep+"/json/" // url do viacep
 	url = `https://viacep.com.br/ws/${cep}/json/` // url do viacep
 
 	// BUSCANDO O CEP USANDO FETCH
 	fetch(url)
-		.then((res)=>{ // variavel "res" irá armazenar a resposta inicial
+		.then((res) => { // variavel "res" irá armazenar a resposta inicial
 			return res.json() // convertendo a resposta em JSON
 		})
-		.then((cep)=>{ // variavel "cep" contendo o json com o CEP do viacep
+		.then((cep) => { // variavel "cep" contendo o json com o CEP do viacep
 			console.log("Oi, meu CEP É no fetch", cep) // imprimindo os dados do cep
 			document.getElementById("cidade").value = cep.localidade
 			document.getElementById("bairro").value = cep.bairro
@@ -20,3 +21,20 @@ function mostrar(){
 	console.log("Oi, meu CEP É fora", cep)
 }
 // tag fechamento do script JS
+
+// Função de buscar por rua
+function mostrarRua() {
+	uf = $("#uf-rua").val()
+	cidade = $("#cidade-rua").val()
+	rua = $("#rua").val()
+
+	url = `https://viacep.com.br/ws/${uf}/${cidade}/${rua}/json/` // url do viacep
+
+	fetch(url)
+		.then((res) => { // variavel "res" irá armazenar a resposta inicial
+			return res.json() // convertendo a resposta em JSON
+		})
+		.then((ruas) => { // variavel "cep" contendo o json com o CEP do viacep
+			console.log("AQUI AS RUAS", ruas) // imprimindo os dados do cep
+		})
+}
